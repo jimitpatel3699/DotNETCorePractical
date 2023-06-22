42092b33becc82e3ee475991c3246e369a18ff5c
jimitpatel3699 <jimitpatel3699@users.noreply.github.com>
2023-06-22 15:28:47 +0530

    Practical20

---
+++ Practical20/Repository/GenericRepository.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Practical20.Models;
+using Practical20.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practical20.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        protected readonly AppDbContext _dbContext;
+
+        protected GenericRepository(AppDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        public async Task<T> GetById(int id)
+        {
+            return await _dbContext.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>> GetAll()
+        {
+            return await _dbContext.Set<T>().ToListAsync();
+        }
+
+        public async Task Add(T entity)
+        {
+            await _dbContext.Set<T>().AddAsync(entity);
+        }
+        [AutoValidateAntiforgeryToken]
+        public void Delete(T entity)
+        {
+            _dbContext.Set<T>().Remove(entity);
+        }
+
+        public void Update(T entity)
+        {
+            _dbContext.Set<T>().Update(entity);
+        }
+
+
+    }
+}
---
+++ Practical20/Repository/UnitOfWork.cs
@@ -0,0 +1,34 @@
+﻿using Practical20.Models;
+using Practical20.Interfaces;
+
+namespace Practical20.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _dbContext;
+        private IStudentRepository _studentRepository;
+
+
+        public UnitOfWork(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public IStudentRepository StudentRepository
+        {
+            get { return _studentRepository = _studentRepository ?? new StudentRepository(_dbContext); }
+        }
+
+        public void Commit()
+            => _dbContext.SaveChanges();
+
+        public async Task CommitAsync()
+            => await _dbContext.SaveChangesAsync();
+
+        public void Rollback()
+            => _dbContext.Dispose();
+
+        public async Task RollbackAsync()
+            => await _dbContext.DisposeAsync();
+    }
+}
