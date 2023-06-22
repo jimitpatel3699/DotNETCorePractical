2334ceabd90426f9d19b5b7ce905b2e71e82311b
jimitpatel3699 <jimitpatel3699@users.noreply.github.com>
2023-06-22 12:25:21 +0530

    Practical19

---
+++ Practical-19/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Practical19.Controllers
+{
+    [Authorize(Roles = "User")]
+    public class UserController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
